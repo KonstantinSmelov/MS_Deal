8a42cd68b5be66f65da6a824cf00fd65c82be592
Konstantin <kostya_smelov@inbox.ru>
2022-06-01 12:59:22 +0300

    in progress

--- src/main/java/neostudy/controller/DealController.java
+++ src/main/java/neostudy/controller/DealController.java
@@ -1,9 +1,15 @@
 package neostudy.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/deal")
 public class DealController {
+
+    @PostMapping("/application")
+    public List<LoanOfferDTO> getOffers(LoanApplicationRequestDTO) {
+        return null;
+    }
 }
