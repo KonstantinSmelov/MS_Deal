3f856d6f5ed561fed13b676d9c7682ab9c0c7263
Konstantin <kostya_smelov@inbox.ru>
2022-06-01 12:52:00 +0300

    develop init

---
+++ src/main/java/neostudy/controller/DealController.java
@@ -0,0 +1,9 @@
+package neostudy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/deal")
+public class DealController {
+}
